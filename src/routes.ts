ab2f73de92c1d6e891b2e3f25e709e4b765a6e17
Raf98 <rafaelrutzsantos@gmail.com>
2021-02-28 05:20:41 -0300

    Added day 4 code

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,20 @@
 import {Router} from 'express';
 import { SurveysController } from './controllers/SurveysController';
 import { UsersController } from './controllers/UsersController';
+import { SendMailController } from './controllers/SendMailConroller';
 
 const router = Router();
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", usersController.create);
-router.get("/users", usersController.show)
+router.get("/users", usersController.show);
 
-router.post("/surveys", surveysController.create)
-router.get("/surveys", surveysController.show)
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export {router};
